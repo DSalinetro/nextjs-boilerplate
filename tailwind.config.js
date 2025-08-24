d4f26ac2a0be504d4a60fcb89a7cb15af6047b66
Danielle Salinetro <dsalinetro@pm.me>
2025-08-24 11:12:38 -0500

    Update tailwind.config.js
    updated code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,6 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 }
