7a697bb3a6f15baee9064f622a3466747e0da079
Danielle Salinetro <dsalinetro@pm.me>
2025-08-26 09:24:04 -0500

    Update next.config.ts
    added short slug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/empathy-audit-pdf',
+        destination: 'https://static1.squarespace.com/static/6894d3a7da06e2603427241f/t/68a874bfd89bb174ca7998d4/1755870399633/Empathy_Audit_Framework_PrintFriendly_v2.pdf',
+        permanent: true, // 308/301 is fine
+      },
+      {
+        source: '/empathy-audit.pdf',
+        destination: 'https://static1.squarespace.com/static/6894d3a7da06e2603427241f/t/68a874bfd89bb174ca7998d4/1755870399633/Empathy_Audit_Framework_PrintFriendly_v2.pdf',
+        permanent: true,
+      },
+    ];
+  },
 };
-
-export default nextConfig;
