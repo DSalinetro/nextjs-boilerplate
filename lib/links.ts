cacbaed41d22b7641e0e535fdb2041b542f59546
Danielle Salinetro <dsalinetro@pm.me>
2025-08-30 15:36:54 -0500

    Update links.ts

--- lib/links.ts
+++ lib/links.ts
@@ -3,7 +3,7 @@ export const LINKS = {
   medium: 'https://medium.com/@dsalinetro',
   linktree: 'https://linktr.ee/daniellesalinetro',
   siteHome: 'https://daniellesalinetro.design',
-  brandingCollection: 'https://dsalinetro.github.io/branding-portfolio/',
+  brandingCollection: '/enhance-branding-portfolio',
   portfolioCollection: 'https://dsalinetro.github.io/danielle-portfolio/',
   // point to your chosen live target
   empathyAudit: 'https://medium.com/@dsalinetro/the-empathy-audit-how-to-evaluate-your-designs-human-impact-267dc8af1bf5',
